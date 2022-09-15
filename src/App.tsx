6a6360188c0b1dc67261c2593932e44fe10ea6c9
felipenmatos <notfelipe2@hotmail.com>
2022-09-15 18:25:47 -0300

    perf: initial project setup and installation of dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import img from "./Assets/e-book.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ContainerText>
+        <TitleText>
+          Aprenda e consulte palavras em Inglês de forma simples e fácil.
+        </TitleText>
+        <Img src={img} />
+      </ContainerText>
+      <ContainerLogin>
+        <TitleLogin>Faça seu Login</TitleLogin>
+      </ContainerLogin>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ContainerText = styled.div`
+  width: 50%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+
+const TitleText = styled.h1`
+  width: 535px;
+  font-size: 28px;
+  line-height: 34px;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const ContainerLogin = styled.div`
+  width: 50%;
+  height: 100vh;
+  background-color: #ffffff;
+`;
+
+const TitleLogin = styled.h1``;
 
 export default App;
